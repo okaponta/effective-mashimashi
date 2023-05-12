f8e1267e61b1ca2a350ccbeec41965caf0cb9e00
Kohei Okamoto <okaponta.19910919@gmail.com>
2023-05-12 22:35:58 +0900

    refactor

--- effective-mashimashi/src/main.rs
+++ effective-mashimashi/src/main.rs
@@ -1,46 +1,44 @@
+use itertools::{iproduct, Itertools};
+
 fn main() {
     println!("| ヤサイ | ニンニク | アブラ | 呪文 | 長さ |");
     println!("|---|---|---|---|---|");
-    for yasai in 0..5 {
-        for ninniku in 0..5 {
-            for abura in 0..5 {
-                let mut res = spell(0, yasai) + &spell(1, ninniku) + &spell(2, abura);
-                for i in 0..5 {
-                    if i == 3 {
-                        continue;
-                    }
-                    let mut tmp = all_spell(i);
-                    if yasai != i {
-                        tmp += &additional_spell(0, yasai);
-                    }
-                    if ninniku != i {
-                        tmp += &additional_spell(1, ninniku);
-                    }
-                    if abura != i {
-                        tmp += &additional_spell(2, abura);
-                    }
-                    if tmp.len() < res.len() {
-                        res = tmp;
-                    }
+    let mut ans = vec![];
+
+    for (yasai, ninniku, abura) in iproduct!(0..5, 0..5, 0..5) {
+        let yna = vec![yasai, ninniku, abura];
+        let mut res = (0..3).into_iter().map(|i| spell(i, yna[i], false)).join("");
+        for all in 0..5 {
+            if all == 3 {
+                continue;
+            }
+            let mut tmp = all_spell(all);
+            for i in 0..3 {
+                if yna[i] != all {
+                    tmp += &spell(i, yna[i], true);
                 }
-                if res.len() == 0 {
-                    res = "ソノママ".to_string();
-                }
-                println!(
-                    "|{}|{}|{}|{}|{}|",
-                    yasai,
-                    ninniku,
-                    abura,
-                    res,
-                    res.chars().count()
-                );
+            }
+            if tmp.len() < res.len() {
+                res = tmp;
             }
         }
+        let mut count = res.chars().count();
+        if count == 0 {
+            res = "ソノママ".to_string();
+            count = 4;
+        }
+        ans.push(format!(
+            "|{}|{}|{}|{}|{}|",
+            yna[0], yna[1], yna[2], res, count
+        ));
+    }
+    for s in ans {
+        println!("{}", s);
     }
 }
 
-fn spell(i: usize, j: usize) -> String {
-    if j == 2 {
+fn spell(i: usize, j: usize, additional: bool) -> String {
+    if !additional && j == 2 {
         return "".to_string();
     }
     let mut res = match i {
@@ -52,6 +50,7 @@ fn spell(i: usize, j: usize) -> String {
     res += match j {
         0 => "ナシ",
         1 => "スクナメ",
+        2 => "フツウ",
         3 => "マシ",
         _ => "マシマシ",
     };
@@ -63,23 +62,6 @@ fn all_spell(i: usize) -> String {
     res += match i {
         0 => "ブナシ",
         1 => "ブスクナメ",
-        3 => "マシ",
-        _ => "マシマシ",
-    };
-    res
-}
-
-fn additional_spell(i: usize, j: usize) -> String {
-    let mut res = match i {
-        0 => "ヤサイ",
-        1 => "ニンニク",
-        _ => "アブラ",
-    }
-    .to_string();
-    res += match j {
-        0 => "ナシ",
-        1 => "スクナメ",
-        2 => "フツウ",
         3 => "マシ",
         _ => "マシマシ",
     };
